0f5766c764a10c1a839be3b1da9ca8a55464c4f0
Jesse Bees <jesse@toomanybees.com>
2018-07-15 01:41:07 -0400

    correctly display depth by copying original histogram fn

--- examples/simple_viewer.rs
+++ examples/simple_viewer.rs
@@ -13,7 +13,33 @@ use openni2::{
     OniRGB888Pixel,
     VideoMode,
     PixelFormat,
+    Frame,
 };
+
+pub fn depth_histogram(hist: &mut [f32], frame: &Frame<OniDepthPixel>) {
+    let pixels = frame.pixels();
+    let mut count = 0usize;
+    for h in hist.iter_mut() {
+        *h = 0f32;
+    }
+
+    for px in pixels {
+        if *px != 0 {
+            hist[*px as usize] += 1.0;
+            count += 1;
+        }
+    }
+
+    for i in 1..hist.len() {
+        hist[i] += hist[i-1];
+    }
+    if count > 0 {
+        for px in hist.iter_mut().skip(1) {
+            *px = 256f32 * (1.0f32 - (*px / count as f32));
+        }
+    }
+}
+
 
 fn main() -> Result<(), Status> {
     openni2::init(2, 2)?;
@@ -32,13 +58,6 @@ fn main() -> Result<(), Status> {
         resolution_y: 240,
         fps: 30,
     })?;
-    // let ir = device.create_stream::<OniRGB888Pixel>(SensorType::IR)?;
-    // ir.set_video_mode(VideoMode {
-    //     pixel_format: PixelFormat::RGB888,
-    //     resolution_x: 320,
-    //     resolution_y: 240,
-    //     fps: 30,
-    // })?;
 
     let mut window = match Window::new("OpenNI2 Simple Viewer", 320, 240, WindowOptions {
         resize: false,
@@ -55,15 +74,19 @@ fn main() -> Result<(), Status> {
     let color_reader = color.reader();
     let depth_reader = depth.reader();
 
+    let mut mirror = color.get_mirroring()?;
     let mut display_color = true;
     let mut display_depth = false;
+    let mut histogram: [f32; 10000] = unsafe { mem::zeroed() };
     let mut buffer: [u32; 320 * 240] = unsafe { mem::zeroed() };
     while window.is_open() && !window.is_key_down(Key::Escape) {
         let color_frame = color_reader.read();
         let depth_frame = depth_reader.read();
+        depth_histogram(&mut histogram, &depth_frame);
         for (i, (color, depth)) in color_frame.pixels().iter().zip(depth_frame.pixels()).enumerate() {
             if display_depth && *depth > 0 {
-                let brightness = (depth / 256) as u32;
+                // let brightness = (depth / 256) as u32;
+                let brightness = histogram[*depth as usize] as u32;
                 buffer[i] = brightness << 16 | brightness << 8 | brightness;
             } else if display_color {
                 buffer[i] = ((color.r as u32) << 16) | ((color.g as u32) << 8) | (color.b as u32);
@@ -89,6 +112,11 @@ fn main() -> Result<(), Status> {
                         display_color = false;
                         display_depth = true;
                         device.set_image_registration(false).unwrap();
+                    },
+                    Key::M => {
+                        color.set_mirroring(!mirror).unwrap();
+                        depth.set_mirroring(!mirror).unwrap();
+                        mirror =! mirror;
                     },
                     _ => (),
                 }
