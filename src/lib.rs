5d8fc562a570d19f6c46a550ee106b57883d8eda
Jess Bees <jesse@toomanybees.com>
2018-07-18 00:52:37 -0400

    crate dox!

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,68 @@
+//! # Rust wrapper for OpenNI2
+//!
+//! OpenNI2 is a library for handling video streams as part of a natural
+//! interaction user interface. Devices such as the Xbox Kinect or Asus
+//! Xtion have drivers recognized by OpenNI2.
+//!
+//! The flow of a program starts with initializing OpenNI2 with `openni2::init()`,
+//! then opening a `Device` and opening video `Stream`s. A stream can be configured
+//! to record a desired `VideoMode`.
+//!
+//! From there, a program can synchronously read frames from a stream, or register
+//! a callback to fire whenever a new frame is ready.
+//!
+//! ## Example
+//!
+//! ```no_run
+//! extern crate openni2;
+//!
+//! use std::{thread, time};
+//! use openni2::{Status, Device, SensorType, StreamReader, OniDepthPixel};
+//!
+//! fn callback(stream: &StreamReader<OniDepthPixel>) {
+//!     // `StreamReader` only has one method, and it's `read`
+//!     let frame = stream.read();
+//!     let px = frame.pixels();
+//!     let closest = px.iter()
+//!         .enumerate()
+//!         .fold((0u16, 0u16, ::std::u16::MAX), |closest, (n, &depth)| {
+//!             let (x, y) = (n as u16 % frame.width(), n as u16 / frame.width());
+//!             if depth < closest.2 && depth != 0 {
+//!                 (x, y, depth)
+//!             } else {
+//!                 closest
+//!             }
+//!     });
+//!     println!("[{:-6} {:-6} {:-6}]", closest.0, closest.1, closest.2);
+//! }
+//!
+//! fn main() -> Result<(), Status> {
+//!     // Initialize the library
+//!     openni2::init()?;
+//!
+//!     // Open the first device we find, or abort early
+//!     let device = Device::open_default()?;
+//!
+//!     // Get a handle for opening a stream from its depth sensor. If the device
+//!     // didn't have a depth sensor, it would return `Err` and abort the program.
+//!     let stream = device.create_stream(SensorType::DEPTH)?;
+//!
+//!     // Register a callback that will be called, with the stream as its first
+//!     // argument, whenever a new frame is ready. When the listener falls out of
+//!     // scope, the callback will be unregistered.
+//!     let _listener = stream.listener(callback)?;
+//!
+//!     // Start the stream, then let the callback run until we kill the program
+//!     // ourselves.
+//!     stream.start();
+//!
+//!     let heartbeat = time::Duration::from_millis(250);
+//!     loop {
+//!         thread::sleep(heartbeat);
+//!     }
+//! }
+//! ```
+
 extern crate openni2_sys;
 use std::os::raw::{c_int, c_void, c_char};
 use std::ffi::{CString, CStr};
